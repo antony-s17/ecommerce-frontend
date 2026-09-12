2b59e36c64625978addbb70907c970e56dd769e3
Antony Salas <salasra1795@gmail.com>
2026-09-11 22:18:28 -0500

    feat: add Components in routes and add authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,131 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-import { getUserProfile } from "./store/authSlice";
-import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import {
+  useDispatch,
+  useSelector,
+} from "react-redux";
 
-import LoginPage from "./pages/LoginPage/LoginPage";
+import {
+  fetchProfile,
+} from "./store/authSlice";
+
+import {
+  fetchCart,
+  clearCart,
+} from "./store/cartSlice";
+
+import {
+  fetchWishlist,
+  clearWishlist,
+} from "./store/wishlistSlice";
+
 import Navbar from "./components/Navbar/Navbar";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import AdminRoute from "./components/AdminRoute/AdminRoute";
-import AdminPage from "./pages/AdminPage/AdminPage";
-import ProductsPage from "./pages/ProductPage/ProductPage";
+
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import ProductsPage from "./pages/ProductsPage/ProductsPage";
 import ProductDetailPage from "./pages/ProductDetailPage/ProductDetailPage";
 import CartPage from "./pages/CartPage/CartPage";
 import WishlistPage from "./pages/WishlistPage/WishlistPage";
 import CheckoutSuccessPage from "./pages/CheckoutSuccessPage/CheckoutSuccessPage";
-
+import AdminPage from "./pages/AdminPage/AdminPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const dispatch = useDispatch();
 
+  const {
+    isAuthenticated,
+  } = useSelector(
+    (state) => state.auth
+  );
+
   useEffect(() => {
-    dispatch(getUserProfile());
+    dispatch(fetchProfile());
   }, [dispatch]);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      dispatch(fetchCart());
+      dispatch(fetchWishlist());
+    }
+  }, [dispatch, isAuthenticated]);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      dispatch(clearCart());
+      dispatch(clearWishlist());
+    }
+  }, [dispatch, isAuthenticated]);
 
   return (
     <BrowserRouter>
       <Navbar />
+
       <Routes>
-        <Route path="/" element={<h1>Inicio</h1>} />
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
 
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<h1>Registro</h1>} />
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
 
-        <Route path="/products" element={<ProductsPage />} />
+        <Route
+          path="/register"
+          element={<RegisterPage />}
+        />
+
+        <Route
+          path="/products"
+          element={<ProductsPage />}
+        />
+
         <Route
           path="/products/:id"
           element={<ProductDetailPage />}
         />
 
         <Route element={<PrivateRoute />}>
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/wishlist" element={<WishlistPage />} />
-          <Route path="/checkout" element={<h1>Checkout</h1>} />
-          <Route path="/checkout/success" element={<CheckoutSuccessPage />}
+          <Route
+            path="/cart"
+            element={<CartPage />}
+          />
+
+          <Route
+            path="/wishlist"
+            element={<WishlistPage />}
+          />
+
+          <Route
+            path="/checkout/success"
+            element={<CheckoutSuccessPage />}
           />
         </Route>
-          <Route element={<AdminRoute />}>
+
+        <Route element={<AdminRoute />}>
+          <Route
+            path="/admin"
+            element={<AdminPage />}
+          />
+        </Route>
         <Route
-          path="/admin"
-          element={<AdminPage />}
+          path="*"
+          element={<NotFoundPage />}
         />
-      </Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
